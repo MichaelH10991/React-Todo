d98578e2e3af65bcfc4ad49fbb89bfd4f2eaa7da
Michael Humphries <michaelhumphries1997@gmail.com>
2019-07-19 19:47:35 +0100

    cleanup

---
+++ src/components/Todos.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class Todos extends Components {
+  render() {
+    return (
+      <div className="App" >
+        <h1>Todos</h1>
+      </div>
+    );
+  }
+}
+
+
+export default Todos;
